0976149a5264d972abd5890ca71a7f787397847b
willroy <willjoroy@gmail.com>
2024-01-21 14:07:43 +0000

    inital commit

---
+++ Game/Objects/Cube-2/Cube2.gd
@@ -0,0 +1,44 @@
+extends MeshInstance
+
+var looking_at = false
+var interacted = false
+var shaderMaterial = null
+
+onready var box = get_node("../Box2")
+
+func _ready():
+	$TextSpatial.hide()
+
+#func _process(delta):
+#	pass
+
+func run_look_at():
+	$TextSpatial.show()
+	
+	var material = box.get_active_material(0)
+	var newMaterial = material.duplicate()
+	box.set_surface_material(0, newMaterial)
+	var shader = load("Game/Shaders/outline.gdshader")
+	shaderMaterial = ShaderMaterial.new()
+	shaderMaterial.shader = shader
+	shaderMaterial.set_shader_param("border_width", 0.1)
+	newMaterial.set_next_pass(shaderMaterial)
+
+func run_look_away():
+	$TextSpatial.hide()
+	
+	var material = box.get_active_material(0)
+	material.set_next_pass(null)
+	shaderMaterial = null
+
+func _input(ev):
+	if looking_at:
+		var player = get_node("../../Player")
+		if Input.is_key_pressed(KEY_E) and looking_at and !interacted:
+			interacted = true
+			player.freeze(true)
+			print("interact")
+		elif Input.is_key_pressed(KEY_Q) and looking_at and interacted:
+			interacted = false
+			player.freeze(false)
+			print("uninteract")
---
+++ Player/Vision.gd
@@ -0,0 +1,28 @@
+extends Timer
+
+var old_overlaps = []
+
+func _ready():
+	pass
+
+#func _process(delta):
+#	pass
+
+func _on_Vision_timeout():
+	var visionArea = get_node("../VisionArea")
+	var overlaps = visionArea.get_overlapping_bodies()
+	if overlaps.size() > 0:
+		for overlap in overlaps:
+			if overlap.get_parent() != null and overlap.get_parent().is_in_group("Interfaces"):
+				var obj = overlap.get_parent()
+				if obj.looking_at != null and obj.looking_at == false:
+					obj.looking_at = true
+					obj.run_look_at()
+
+		for old_overlap in old_overlaps:
+			if !overlaps.has(old_overlap) and old_overlap.get_parent() != null and old_overlap.get_parent().is_in_group("Interfaces"):
+				var obj = old_overlap.get_parent()
+				if obj.looking_at != null and obj.looking_at == true:
+					obj.looking_at = false
+					obj.run_look_away()
+	old_overlaps = overlaps
